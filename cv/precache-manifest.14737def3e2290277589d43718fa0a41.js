427eb77d9afcd8022a6d79b7b21c506cdea5e52c
carathorys <gallayb@gmail.com>
2019-05-13 23:38:36 +0200

    CV added

---
+++ cv/precache-manifest.14737def3e2290277589d43718fa0a41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1260bbd9b9b8ef99b20559716fac61",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "10fd5c8305729fc69306",
+    "url": "/cv/static/css/main.65cb9238.chunk.css"
+  },
+  {
+    "revision": "f95e8522f6b2515c4cb0",
+    "url": "/cv/static/js/2.169619be.chunk.js"
+  },
+  {
+    "revision": "10fd5c8305729fc69306",
+    "url": "/cv/static/js/main.3cdf9151.chunk.js"
+  },
+  {
+    "revision": "18079a4bdaee1d56978c",
+    "url": "/cv/static/js/runtime~main.6f651ed7.js"
+  },
+  {
+    "revision": "85f2546e1e8a9a97c05ad11a9e4a6ece",
+    "url": "/cv/static/media/bg1.85f2546e.jpg"
+  }
+]);
