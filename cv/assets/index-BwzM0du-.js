ee21e4a43740de178fe835f669cdc32f5d4bfce1
carathorys <gallayb@gmail.com>
2024-05-30 03:01:44 +0000

    Updated CV files from ref 2e621578632ff3fdb5dec8f1ed7de37f9fa8770a

---
+++ cv/assets/index-BwzM0du-.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DEoz_jdz.js";function r(){return t.jsx(t.Fragment,{children:"Skills"})}export{r as default};
--- cv/assets/index-h7-Zmfte.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-zg6jFx60.js";function t(){return e.jsx(e.Fragment,{children:"Home"})}export{t as default};
